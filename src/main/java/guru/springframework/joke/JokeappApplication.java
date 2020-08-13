f608f12447ad4b1220d72b1655fac16d8c5bcc63
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2020-08-12 21:43:35 -0400

    S04-L57 Creating the Spring Service Layer

--- src/main/java/guru/springframework/joke/JokeappApplication.java
+++ src/main/java/guru/springframework/joke/JokeappApplication.java
@@ -9,5 +9,4 @@ public class JokeappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JokeappApplication.class, args);
 	}
-
 }
---
+++ src/main/java/guru/springframework/joke/services/JokeService.java
@@ -0,0 +1,9 @@
+package guru.springframework.joke.services;
+
+/**
+ * Created by dmitri on 2020-08-12.
+ */
+public interface JokeService {
+
+    String getJoke();
+}
